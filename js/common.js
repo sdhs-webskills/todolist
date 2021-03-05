95f36f21049777a0820e10f5a995d020be1b49a5
kimjuneseo <june3929213512@gmail.com>
2021-03-05 16:30:52 +0900

    03-05

--- js/common.js
+++ js/common.js
@@ -1,1 +1,13 @@
-const
+// window.onload() = function(){
+const mainInput = document.querySelector('#main');
+const todoList = document.querySelector('list');
+
+mainInput.addEventListener('keydown',function(e){
+    if(e.keyCode == 13){
+        let li = document.createElement('li')
+        let todoElemnt = li.classList.add('controll')
+        let hh = document.todoList.appendChild(todoElemnt)
+        // console.log(1)
+    }
+})
+// }
