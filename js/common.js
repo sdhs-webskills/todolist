386d8751b075c3ff7f213ad96dcf538232a466b8
wnsdnn <a24369029@gmail.com>
2021-03-10 16:29:53 +0900

    하나 남음

--- js/common.js
+++ js/common.js
@@ -4,11 +4,11 @@ const li  = document.querySelector("li.list");
 const controll = document.querySelector("#controll");
 let count = document.querySelector("#item");
 
+const allCheck = document.querySelector("#all_sel");
 const allBtn = document.querySelector("#all");
 const activeBtn = document.querySelector("#act");
 const completedBtn = document.querySelector("#com");
 const clearBtn = document.querySelector("#cle");
-const allCheck = document.querySelector("#all_sel");
 
 const Menu = function(){
     if(list.children.length > 1) {
@@ -31,44 +31,146 @@ const Create = function(tag){
 };
 
 const Count = function(){
-    return count.innerHTML = list.children.length - 1;
+    const blockList = [...list.children].filter(function(ele){
+        return !ele.classList.contains("li_ac");
+    })
+
+    blockList.pop();
+
+    return count.innerHTML = blockList.length;
 };
 
+const allCount = function(){
+    const blockList = [...list.children].filter(function(ele){
+        return ele.classList.contains("list");
+    })
+
+    return count.innerHTML = blockList.length;
+};
+
+
 const ActCount = function(){
-    let pos = 0;
-    [...list.children].forEach(function(ele){
-        if(!ele.classList.contains("li_ac")){
-            pos++;
-        }
-        count.innerHTML = pos - 1;
-    });
+    const blockList = [...list.children].filter(function(ele){
+        return !ele.classList.contains("li_ac");
+    })
+
+    blockList.pop();
+
+    count.innerHTML = blockList.length;
 };
 
 const ComCount = function(){
-    let pos = 0;
-    [...list.children].forEach(function(ele){
-        if(ele.classList.contains("li_ac")){
-            pos++;
-        }
-        count.innerHTML = pos;
-    });
+    const blockList = [...list.children].filter(function(ele){
+        return ele.classList.contains("li_ac");
+    })
+
+    count.innerHTML = blockList.length;
 };
 
 
-clearBtn.addEventListener("click", function(){
+const checkBox =  function({ target }){
+    if(target.classList.contains("check")) {
+        // contains(): 지정한 클래스 값이 엘리먼트의 class 속성에 존재하는지 확인한다.
+        // target된 곳에 class가 "check"이면 밑에 있는 거 실행
+        target.parentNode.classList.toggle("li_ac");
+        // target된 곳에 부모 요소에 class 추가
 
-    //clearBtn버튼 클릭시 완료된 모든 리스트 삭제 시키는 것
-    [...document.querySelectorAll(".li_ac")].forEach((list) => {
-        list.remove();
-    });
-    allBtn.click();
-    Menu();
-});
+        // 체크표시 해놓는거
+        if(target.parentNode.classList.contains("li_ac")){
+            target.parentNode.children[0].innerText = `✓`;
+        } else {
+            target.parentNode.children[0].innerText = ``;
+        }
+
+        const button = document.querySelector(".sp_ac");
+        const id = button.id;
+
+        switch(id) {
+            case "all" : allBtn.click();
+                break;
+            case "act" : activeBtn.click();
+                break;
+            case "com" : completedBtn.click();
+                break;       
+        };
+    };
+
+    
+    // 모든 체크박스가 선택되면 전체선택 되게 하는거
+    let lists = [...list.children].filter(function(ele){
+        return ele.classList.contains("li_ac")
+    })
+
+    if(lists.length === list.children.length-1){
+        allCheck.classList.add("all_ac");
+    } else {
+        allCheck.classList.remove("all_ac");
+    }
+
+};
+
+// 리스트 더블 클릭시 수정 하게 하는 거
+const dblClick =  function({ target }){
+    
+    if(target.classList.contains("text")){
+        // input 생성
+        let inputText = Create("input");
+        
+        // this.children에 class가 text인것만 telist에 저장
+        let telist = [...this.children].filter(function(ele){
+            return ele.classList.contains("text"); // 어차피 1개
+        });
+        
+        // 만들어 놓은 input에 원래 p값 집어넣기
+        inputText.value = telist[0].innerText;
+        
+        inputText.classList.add("text");
+        
+        // p태그 지우기
+        this.children[1].remove();
+        
+        
+        // this에 자식중 0번쨰 다음에 input 집어 넣기 
+        this.children[0].after(inputText);
+        
+        // 더블 클릭시 발동
+        inputText.addEventListener("keydown", function({ key }){
+            // 엔터키를 눌렀을때
+            if(key == "Enter"){
+                // 넣을 p 태그 생성
+                let p = Create("p");
+                
+                // p 태그에 input 값 넣기, 클래스명 넣기
+                p.innerHTML = this.value;
+                p.classList.add("text");
+        
+                // input을 첫번째 자식뒤에 넣고, 첫번쨰 자식 지우기
+                this.parentNode.children[1].after(p);
+                this.parentNode.children[1].remove();
+            }
+            // Esc를 눌렀을때
+            if(key == "Escape"){
+                let p = Create("p");
+        
+                // p 태그에 바꾸기 전 input값 그대로 넣기 
+                p.innerHTML = telist[0].innerText;
+                p.classList.add("text");
+        
+            
+                // input을 첫번째 자식뒤에 넣고, 첫번쨰 자식 지우기
+                this.parentNode.children[1].after(p);
+                this.parentNode.children[1].remove();
+            }
+        });
+    }
+
+}
 
 
 allBtn.addEventListener("click", function(){
+    Border(this);
 
-    //allBtn버튼 클릭시 모든 리스트를 표시 시키는거
+    // allBtn버튼 클릭시 모든 리스트를 표시 시키는거
     [...list.children].forEach(function(ele){
         if(ele.classList.contains("list")){
             ele.style.display = "block";
@@ -76,14 +178,18 @@ allBtn.addEventListener("click", function(){
             ele.style.display = "none";
         }
     });
-    Count();
-    Border(this);
+    list.addEventListener("click", checkBox);
+    allCount();
     Menu();
 });
 
 activeBtn.addEventListener("click", function(){
+    Border(this);
+    if(this.classList.contains("sp_ac")){
 
-    //activeBtn버튼 클릭시 완료되지 않는 것만 표시 시키는거
+    }
+
+    // activeBtn버튼 클릭시 완료되지 않는 것만 표시 시키는거
     [...list.children].forEach(function(ele){
         if(ele.classList.contains("li_ac")){
             ele.style.display = "none";
@@ -91,15 +197,16 @@ activeBtn.addEventListener("click", function(){
             ele.style.display = "block";
         }
     });
+    list.addEventListener("click", checkBox);
     ActCount();
-    Border(this);
     Menu();
 });
 
 
 completedBtn.addEventListener("click", function(){
+    Border(this);
 
-    //completedBtn버튼 클릭시 완료된것만 표시 시키는거
+    // completedBtn버튼 클릭시 완료된것만 표시 시키는거
     [...list.children].forEach(function(ele){
         if(!ele.classList.contains("li_ac")){
             ele.style.display = "none";
@@ -107,11 +214,20 @@ completedBtn.addEventListener("click", function(){
             ele.style.display = "block";
         }
     });
+    list.addEventListener("click", checkBox);
     ComCount();
-    Border(this);
     Menu();
 });
 
+clearBtn.addEventListener("click", function(){
+    
+    // clearBtn버튼 클릭시 완료된 모든 리스트 삭제 시키는 것
+    [...document.querySelectorAll(".li_ac")].forEach((list) => {
+        list.remove();
+    });
+    allBtn.click();
+    Menu();
+});
 
 input.addEventListener("keydown", function({ key }){
     // input에세 엔터시 리스트 추가 하는거 
@@ -149,68 +265,15 @@ input.addEventListener("keydown", function({ key }){
             this.classList.remove("hov");
         });
 
-        //리스트 더블 클릭시 수정 하게 하는 거 [미완성]
-        li.addEventListener("dblclick", function({ target }){
-            
-            let inputText = Create("input");
-            
-            let telist = [...this.children].filter(function(ele){
-                return ele.classList.contains("text");
-            })
-            
-            inputText.value = telist[0].innerText;
-
-            this.children[1].remove();
-
-            this.children[0].after(inputText);
-
-            inputText.addEventListener("keydown", function({ key }){
-                if(key == "Enter"){
-                    // let p = Create("p");
-                    // p.innerHtml = inputText.value;
-                    // // console.log(li.children[1])
-                    // li.children[1].remove();
-                    // li.children[0].after(p);
-                }
-            });
-
-            console.log(this.children[1]);
-
-        });
-
+        li.addEventListener("dblclick", dblClick);
+        
         allBtn.click();
     }
     Menu();
 });
 
 
-list.addEventListener("click", function({ target }){
-    if(target.classList.contains("check")) {
-        // contains(): 지정한 클래스 값이 엘리먼트의 class 속성에 존재하는지 확인한다.
-        // target된 곳에 class가 "check"이면 밑에 있는 거 실행
-        target.parentNode.classList.toggle("li_ac");
-        // target된 곳에 부모 요소에 class 추가
 
-        // 체크표시 해놓는거
-        if(target.parentNode.classList.contains("li_ac")){
-            target.parentNode.children[0].innerText = `✓`;
-        } else {
-            target.parentNode.children[0].innerText = ``;
-        }
-    };
-
-
-    let lists = [...list.children].filter(function(ele){
-        return ele.classList.contains("li_ac")
-    })
-
-    // 모든 체크박스가 선택되면 전체선택 되게 하는거
-    if(lists.length === list.children.length-1){
-        allCheck.classList.add("all_ac");
-    } else {
-        allCheck.classList.remove("all_ac");
-    }
-});
 
 allCheck.addEventListener("click", function(){
     
