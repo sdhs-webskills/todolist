4c9d1f9a1e0bba448c6305c125e1a73078fa4d05
junilHwang <junil.h@kakao.com>
2021-08-26 18:52:29 +0900

    옵저버 패턴 만들기

--- src/utils/debounce.js
+++
@@ -1,7 +0,0 @@
-export function debounce (callback) {
-  let currentTimer = -1;
-  return function () {
-    cancelAnimationFrame(currentTimer);
-    currentTimer = requestAnimationFrame(callback);
-  }
-}
